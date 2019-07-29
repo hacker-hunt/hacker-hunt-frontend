79eb29bf345ca03fc6d25e12f0ce0b82bc448ed7
Talent Antonio <antonio.talenty@gmail.com>
2019-07-29 11:43:05 +0100

    header and logo

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMap, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import logo from '../img/logo.png';
+import { NavLink } from 'react-router-dom';
 
 const HeaderComponent = () => {
   return (
-      <HeaderWrapper>
-        <h1>Hacker Hunter</h1>
-      </HeaderWrapper>
-
+    <HeaderWrapper>
+      <div className="container">
+        <div className="logo">
+          <img src={logo} alt="hacker" />
+        </div>
+        <h1>Treasure Hunt</h1>
+        <nav>
+          <NavLink exact to="/">
+            <FontAwesomeIcon icon={faMap} />
+            <span>Map</span>
+          </NavLink>
+          <NavLink to="/about">
+            <FontAwesomeIcon icon={faInfoCircle} />
+            <span>About</span>
+          </NavLink>
+        </nav>
+      </div>
+    </HeaderWrapper>
   );
 };
 
 export default HeaderComponent;
 
 const HeaderWrapper = styled.div`
-  text-align: center;
+height: 60px;
+
 `;
