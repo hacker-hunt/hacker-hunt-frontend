7351bdf41db9225495e9f15a042c0f78022735ff
Talent Antonio <antonio.talenty@gmail.com>
2019-07-30 09:21:46 +0100

    room showcase

--- src/components/SidebarComponent.js
+++ src/components/SidebarComponent.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import ProgressBar from './ProgressBar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDollarSign } from '@fortawesome/free-solid-svg-icons';
+import { PlayerLoader } from './Loaders';
+
+/*
+This component displays most of the information retrieved from the API.
+*/
 
 const SidebarComponent = ({
-  title,
-  players
+title,
+players,
+room
 }) => {
   return (
     <StyledSidebar>
-      
+      <div className="room">
+        <>
+          <h2 className="room-id">
+            Room 
+          </h2>
+
+          </>
+      </div>
     </StyledSidebar>
   );
 };
 
 const StyledSidebar = styled.div`
   width: 25%;
-  height: 600%;
-  padd
+  padding:0.5rem;
+  height: 100%;
   display: flex;
   flex-direction: column;
-  background: #d3e5e5;
-  animation: 1s ease-in-out;
+  background: #fff;
+  
 `;
 
 export default SidebarComponent;
