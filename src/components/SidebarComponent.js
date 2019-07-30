a9205288f9f9e7a29fc15ed9cf2a952a121106ca
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-07-30 19:49:55 +0100

    Remove unused components

--- src/components/SidebarComponent.js
+++ src/components/SidebarComponent.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import ProgressBar from './ProgressBar';
-import { PlayerLoader } from './Loaders';
 
 const SidebarComponent = (props) => {
   const { description, roomId, coordinates, title, items, players, gold, encumbrance, strength, speed, inventory } = props;
