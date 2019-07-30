c38bc8b810f9536756e81c733d64c4d32b1619f3
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-07-30 09:21:48 +0100

    Add everything that needs to be tracked to state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import HeaderComponent from './components/HeaderComponent';
 import MainComponent from './components/MainComponent';
-import SidebarComponent from './components/SidebarComponent';
 import FooterComponent from './components/FooterComponent';
 
+import mapGraph from './data.json';
 import './App.css';
 
-function App() {
-  return (
-    <AppWrapper>
-      <HeaderComponent />
-      <MainComponent />
-      <FooterComponent />
-    </AppWrapper>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mapGraph: mapGraph,
+      room_id: null,
+      title: '',
+      description: '',
+      coordinates: '(60, 60)',
+      elevation: null,
+      terrain: '',
+      players: [],
+      items: [],
+      exits: [],
+      cooldown: null,
+      errors: [],
+      messages: [],
+      name: '',
+      gold: 0,
+      inventory: [],
+      encumbrance: null,
+      strength: null,
+      speed: null,
+      status: [],
+      isExploring: false
+    };
+  }
+
+
+
+  render() {
+    const { mapGraph, messages } = this.state;
+    return (
+        <AppWrapper>
+          <HeaderComponent />
+          <MainComponent mapGraph={mapGraph} />
+          <FooterComponent messages={messages}/>
+        </AppWrapper>
+    );
+  }
 }
 
 export default App;
@@ -24,5 +56,6 @@ const AppWrapper = styled.div`
   flex-direction: column;
   justify-content: space-between;
   height: 100vh;
+  min-height: 100vh;
   background: #34314F;
 `;
