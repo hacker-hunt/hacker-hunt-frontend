bdbd5fb2166c761d4736796da040c9aa020c1813
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-07-30 09:17:16 +0100

    Create footer component that will later help the player navigate rooms and gather items

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const FooterComponent = () => {
+import CardinalsComponent from './CardinalsComponent';
+
+const FooterComponent = (props) => {
+  const { messages, isExploring } = props;
   return (
       <FooterWrapper>
-        <h1>Footer</h1>
+        <CardinalsComponent />
+        <div className="action-icons">
+          <i className="fas fa-store-alt" />
+          <i className="fas fa-dollar-sign" />
+          <i className="fas fa-hand-paper" />
+        </div>
+        <div className="messages">
+          <button onClick={() => console.log('Exploring...')}>{isExploring ? 'EXPLORING...' : 'EXPLORE'}</button>
+          <div>
+            {!messages.length ? (
+              <p>Click EXPLORE to start exploring.</p>
+            ) : (
+              messages.map(message => (
+                  <p>{message}</p>
+              ))
+            )}
+          </div>
+        </div>
       </FooterWrapper>
   );
 };
@@ -14,4 +34,40 @@ export default FooterComponent;
 const FooterWrapper = styled.div`
   text-align: center;
   background: #692DB7;
+  display: flex;
+  align-items: center;
+  .action-icons {
+    i {
+      padding: 2rem;
+      font-size: 3rem;
+      color: #FFFFFF;
+      &:hover {
+        cursor: pointer;
+        color: #d7d5e5;
+        transition: 0.5s ease;
+      }
+    }
+  }
+  
+  .messages {
+    display: flex;
+    align-items: center;
+    p {
+      font-size: 2rem;
+      color: #FFFFFF;
+      padding-left: 1.5rem;
+    }
+    button {
+      padding: 1rem 2rem;
+      border-radius: 4px;
+      font-size: 2rem;
+      color: #34314F;
+      font-weight: 500;
+      &:hover {
+        cursor: pointer;
+        background: #d7d5e5;
+        transition: 0.5s ease;
+      }
+    }
+  }
 `;
