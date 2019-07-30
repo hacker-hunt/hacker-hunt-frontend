2d1e7aca8c7f022e5a297a4ce48e7297ee574e30
Talent Antonio <antonio.talenty@gmail.com>
2019-07-30 12:45:49 +0100

    content

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-class AboutComponent extends Component{
-    
+class AboutComponent extends Component {
+    render(){
+        return (
+            <div className="container">
+            <h2>About</h2>
+            <p>Welcome to Team Lukas Treasure Hunt!</p>
+            <p>
+            Something about the game
+            </p>
+            <p>Let the games begin</p>
+            </div>
+    );
+  }
 }
 export default AboutComponent;
