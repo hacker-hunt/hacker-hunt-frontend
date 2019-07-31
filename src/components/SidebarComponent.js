668796da672b1bf76957fbbe843f0881dffd3b80
mark-marshall <mark.marshallgp@gmail.com>
2019-07-31 15:18:11 +0100

    Fixed sidebar duplication bug

--- src/components/SidebarComponent.js
+++ src/components/SidebarComponent.js
@@ -1,55 +1,63 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const SidebarComponent = (props) => {
-  const { description, roomId, coordinates, title, items, players, gold, encumbrance, strength, speed, inventory, cooldown } = props;
+const SidebarComponent = props => {
+  const {
+    description,
+    roomId,
+    coordinates,
+    title,
+    items,
+    players,
+    gold,
+    encumbrance,
+    strength,
+    speed,
+    inventory,
+    cooldown,
+  } = props;
   return (
-      <SidebarWrapper>
-        <div className="room-info">
-          <p>Cooldown: {cooldown}</p>
-          <h1>Room {roomId}<span> {coordinates}</span></h1>
-          <h2>{title}</h2>
-          <p>{ description }</p>
-          <h2>Items</h2>
-          {!items.length ? (
-              <p>There are no items in this room.</p>
-          ) : (
-              items.map(item => (
-                  <p key={roomId}>{item}</p>
-              ))
-          )}
+    <SidebarWrapper>
+      <div className="room-info">
+        <p>Cooldown: {cooldown}</p>
+        <h1>
+          Room {roomId}
+          <span> {coordinates}</span>
+        </h1>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <h2>Items</h2>
+        {!items.length ? (
+          <p>There are no items in this room.</p>
+        ) : (
+          items.map(item => <p>{item}</p>)
+        )}
+      </div>
+
+      <div className="player-info">
+        <h2>Players</h2>
+        {!players.length ? (
+          <p>There are no players in this room.</p>
+        ) : (
+          players.map(player => <p>{player}</p>)
+        )}
+        <div className="fortune">
+          <h2>Gold</h2>
+          <span> $ {gold}</span>
         </div>
 
-        <div className="player-info">
-          <h2>Players</h2>
-          {!players.length ? (
-              <p>There are no players in this room.</p>
-          ) : (
-              players.map(player => (
-                  <p key={roomId}>{player}</p>
-              ))
-          )}
-          <div className="fortune">
-            <h2>Gold</h2>
-            <span> $ {gold}</span>
-          </div>
-
-          <div className="powers">
-            <p>Encumbrance: {encumbrance}</p>
-            <p>Strength: {strength}</p>
-            <p>Speed: {speed}</p>
-            <p>Inventory: {inventory}</p>
-            {!inventory.length ? (
-                <p>Your inventory is currently empty</p>
-            ) : (
-                inventory.map(inventoryItem => (
-                    <p key={roomId}>{inventoryItem}</p>
-                ))
-            )}
-          </div>
+        <div className="powers">
+          <p>Encumbrance: {encumbrance}</p>
+          <p>Strength: {strength}</p>
+          <p>Speed: {speed}</p>
+          <p>Inventory: {
+            inventory.length
+            ? inventory.map(inventoryItem => <p>{inventoryItem}</p>)
+            : "Your inventory is empty"
+          }</p>
         </div>
-
-      </SidebarWrapper>
+      </div>
+    </SidebarWrapper>
   );
 };
 
@@ -59,24 +67,24 @@ const SidebarWrapper = styled.div`
   width: 300px;
   padding: 1.5rem;
   h1 {
-    color: #34314F;
+    color: #34314f;
     font-family: 'Changa', sans-serif;
     font-size: 2.4rem;
     font-weight: 700;
   }
   p {
     font-size: 1.6rem;
-    color: #34314F
+    color: #34314f;
   }
   span {
-    color: #692DB7;
+    color: #692db7;
   }
-  
+
   h2 {
     font-size: 2rem;
     font-weight: 700;
     padding: 1rem 0;
-    color: #34314F
+    color: #34314f;
   }
   .fortune {
     display: flex;
@@ -84,7 +92,7 @@ const SidebarWrapper = styled.div`
     align-items: center;
     span {
       font-size: 1.6rem;
-      color: #34314F
+      color: #34314f;
     }
   }
 `;
